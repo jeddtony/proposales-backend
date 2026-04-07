a7abffbd92e894a72d45793e7c3238bccdb4e6bc
Jed <jedtony.jt@gmail.com>
2026-04-07 19:45:16 +0200

    Fix cookie issue

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -16,7 +16,7 @@ const createToken = (user: User): TokenData => {
 };
 
 const createCookie = (tokenData: TokenData): string => {
-  return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn};`;
+  return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}; SameSite=None; Secure`;
 };
 
 @Service()
